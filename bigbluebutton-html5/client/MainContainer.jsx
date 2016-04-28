f006878e4cc768c3a66ef2c1241c1e06cb5532c5
Lucas Zawacki <lfzawacki@gmail.com>
2016-04-28 17:36:32 -0400

    Use react-a11y as an accesibility linter

--- bigbluebutton-html5/client/MainContainer.jsx
+++ bigbluebutton-html5/client/MainContainer.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import a11y from 'react-a11y';
 import {Header} from '/imports/ui/main/Header.jsx';
 import {Whiteboard} from '/imports/ui/whiteboard/Whiteboard.jsx';
 import {Chat} from '/imports/ui/chat/Chat.jsx';
+
+// Accessibility "linter" for React components
+a11y(React);
 
 MainContainer = React.createClass({
   handleShield() {
